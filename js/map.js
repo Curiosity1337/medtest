bef410b1e15ccd95f942110ffb897d6fefab6a72
Curiosity1337 <axaxaxalol@gmail.com>
2018-07-05 23:09:26 +0300

    border fix and map.js

--- js/map.js
+++ js/map.js
@@ -1,15 +1,70 @@
-function initMap() {
+function myMap() {
     var coordinates = {lat: 59.904097, lng: 29.089236},
 
-        map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('googleMap'), {
             center: coordinates,
-            zoom: 10,
+            zoom: 16,
             disableDefaultUI: true,
-            scrollwheel: true
+            scrollwheel: true,
+            styles: [
+                {
+                    "featureType": "administrative",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#167eab"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "poi",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#167eab"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "geometry.fill",
+                    "stylers": [
+                        {
+                            "color": "#76f8c4"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "road",
+                    "elementType": "labels.text.fill",
+                    "stylers": [
+                        {
+                            "color": "#22a8e1"
+                        }
+                    ]
+                },
+                {
+                    "featureType": "water",
+                    "stylers": [
+                        {
+                            "color": "#7dccee"
+                        }
+                    ]
+                }
+            ]
+
         });
     marker = new google.maps.Marker({
         position: coordinates,
         map: map,
-        animation: google.maps.Animation.BOUNCE
+        icon: 'img/marker.png',
+        animation: google.maps.Animation.DROP
+
     });
+
+    infowindow.open(map,marker);
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
 }
